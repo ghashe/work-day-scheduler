79495f44cfe4cc6174141c17feb47f1e4441a085
Abyou <emandaabyou@gmail.com>
2022-04-07 18:23:15 -0500

    A commit for declaring variables for each schedule's hours.

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+// Declaring today’s date and present hour variables
+var currentDate =
+  moment().format("dddd") + " " + moment().format("Do MMM YYYY");
+var presentHour = moment().format("h:mm:ss a");
+
+// Assigning variables for each our
+
+// First shift
+firstShift9 = $("#first_shift_9");
+firstShift10 = $("#first_shift_10");
+firstShift11 = $("#first_shift_11");
+firstShift12 = $("#first_shift_12");
+firstShift1 = $("#first_shift_1");
+firstShift2 = $("#first_shift_2");
+firstShift3 = $("#first_shift_3");
+firstShift4 = $("#first_shift_4");
+firstShift5 = $("#first_shift_5");
+
+// Second shift
+secondShift5 = $("#second_shift_5");
+secondShift6 = $("#second_shift_6");
+secondShift7 = $("#second_shift_7");
+secondShift8 = $("#second_shift_8");
+secondShift9 = $("#second_shift_9");
+secondShift10 = $("#second_shift_10");
+secondShift11 = $("#second_shift_11");
+secondShift12 = $("#second_shift_12");
+secondShift1 = $("#second_shift_1");
+
+// Third shift
+thirdShift12 = $("#third_ shift_12");
+thirdShift1 = $("#third_shift_1");
+thirdShift2 = $("#third_shift_2");
+thirdShift3 = $("#third_shift_3");
+thirdShift4 = $("#third_shift_4");
+thirdShift5 = $("#third_shift_5");
+thirdShift6 = $("#third_shift_6");
+thirdShift7 = $("#third_shift_7");
+thirdShift8 = $("#third_shift_8");
